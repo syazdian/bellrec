54dcbdc887b77ff445201e0c75b7707d44780410
sharif1.yazdian1 <a.mostafa@chmail.ir>
2023-05-17 16:50:00 +0330

    remove unnecessary codes

--- PWA/Common/Models/CompareBellStapleCellPhone.cs
+++ PWA/Common/Models/CompareBellStapleCellPhone.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bell.Reconciliation.Common.Models;
+﻿namespace Bell.Reconciliation.Common.Models;
     public class CompareBellStapleCellPhone
     {
         public MatchStatus MatchStatus { get; set; }
