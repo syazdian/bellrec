60897c1ec1e2898776e930e3592a263f9736a5f6
sharif.yazdian@outlook.com <sharif.yazdian@outlook.com>
2023-05-29 17:28:46 -0400

    remove desktop folser entirely

--- Client/Desktop/Desktop.Data/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Design;
-global using Bell.Reconciliation.Common.Models;
--- Client/Desktop/Desktop.Data/ProjectConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Bell.Reconciliation.Frontend.Desktop.Data;
-
-public static class ProjectConfig
-{
-    public static string DatabasePath => $"Filename={Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\BellRec.db";
-}
