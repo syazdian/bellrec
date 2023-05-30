03677dcb00d11c74854a21fab4a21ea37adeedba
Sharif Yazdian <Sharif.Yazdian@staples.ca>
2023-05-29 20:03:30 -0400

    make sqlite work

--- Server/Web.Server/Services/ServerDbRepository.cs
+++ Server/Web.Server/Services/ServerDbRepository.cs
@@ -1,5 +1,5 @@
 ﻿using Bell.Reconciliation.Common.Models;
-using Bell.Reconciliation.Web.Server.Data.Sqlserver;
+using Bell.Reconciliation.Web.Server.Data.sqlite;
 using Mapster;
 using Microsoft.EntityFrameworkCore;
 using System.Xml;
@@ -66,40 +66,5 @@ namespace Bell.Reconciliation.Web.Server.Services
             var adapted = items.Adapt<List<StaplesSourceDto>>();
             return adapted;
         }
-
-        #region MAY BE OBSELETE
-
-        public async Task<List<BellSourceDto>> BellSourceGeneratorFromMemory(int startId = 0)
-        {
-            List<BellSourceDto> bellSources = new();
-
-            for (int i = startId + 1; i < startId + 1000; i++)
-            {
-                bellSources.Add(await GeneratoreBellSourceObject(i));
-            }
-            return bellSources;
-        }
-
-        public async Task<BellSourceDto> GeneratoreBellSourceObject(int i)
-        {
-            return await Task.Run(() =>
-            {
-                var newBell = new Common.Models.BellSourceDto()
-                {
-                    //Id = i,
-                    //Phone = ((long)new Random().Next(1000000, 9999999)),
-                    //Amount = ((long)new Random().Next(1, 1000))
-                    //CommissionDetails = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
-                    //CustomerName = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
-                    //IMEI = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
-                    //LOB = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
-                    //OrderNumber = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
-                    //TransactionDate = DateTime.Now.AddDays(new Random().Next(-100000, 0)).ToShortDateString(),
-                };
-                return newBell;
-            });
-        }
-
-        #endregion MAY BE OBSELETE
     }
 }
