b7e45726b7d3542ae34f7431873d028e0b305318
Sharif Yazdian <Sharif.Yazdian@staples.ca>
2023-05-30 13:50:13 -0400

    fixing address

--- Client/Web/Services/Services/FilterService.cs
+++ Client/Web/Services/Services/FilterService.cs
@@ -20,28 +20,8 @@ public class FilterService : IFilterService
     {
         try
         {
-            //var dir = Path.GetDirectoryName(typeof(FilterService).Assembly.Location)!;
-            //var response = File.ReadAllText(Path.Combine(dir, "Data", "filteritems.txt"));
-
             //var response = await new HttpClient().GetStringAsync("https://localhost:7131/api/FilterValue/GetFilterItems");
             var response = await _httpClient.GetFromJsonAsync<FilterItems>($"/api/FilterValue/GetFilterItems");
-
-            //var response = await httpClient.GetStringAsync("/api/FilterValue/GetFilterItems");
-
-            // string response = "{\"loBs\":[{\"name\":\"Wireless\",\"subLoBs\":[\"Wireless\",\"IoT\",\"Mobile Data\"]},{\"name\":\"Wireline\",\"subLoBs\":[\"Home Internet\",\"Home Television\",\"Home Phone\",\"SMB Internet\"]}],\"locations\":[\"Canada\"],\"brands\":[\"Bell\",\"Virgin\",\"Lucky Mobile\"],\"rebateTypes\":[\"Commission\",\"DownPayment\",\"Tax\"]}";
-
-            //var path = Directory.GetFiles("C:\\Users\\Public");
-            //using (StreamReader sr = File.OpenText("C:\\Users\\Public\\filteritems.txt"))
-            //{
-            //    response = sr.ReadToEnd();
-            //}
-            // var response = File.ReadAllText("filteritems.txt");
-            //FilterItems filterItems = new FilterItems();
-
-            //if (!string.IsNullOrEmpty(response))
-            //{
-            //    filterItems = response.JsonDeserialize<FilterItems>();
-            //}
 
             return response;
         }
