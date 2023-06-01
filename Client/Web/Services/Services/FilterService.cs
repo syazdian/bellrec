146600234547b5874ee4c4f5eb42871052bd0256
Sharif <syazdian@live.com>
2023-06-01 16:01:10 +0000

    Merged PR 16: add base url

--- Client/Web/Services/Services/FilterService.cs
+++ Client/Web/Services/Services/FilterService.cs
@@ -3,6 +3,7 @@ using Bell.Reconciliation.Common.Utilities;
 using Bell.Reconciliation.Frontend.Shared.ServiceInterfaces;
 using System.Net.Http.Json;
 using System.Reflection;
+using System.Text.Json;
 
 namespace Bell.Reconciliation.Frontend.Web.Services;
 
@@ -20,30 +21,25 @@ public class FilterService : IFilterService
     {
         try
         {
-            //var dir = Path.GetDirectoryName(typeof(FilterService).Assembly.Location)!;
-            //var response = File.ReadAllText(Path.Combine(dir, "Data", "filteritems.txt"));
-
             //var response = await new HttpClient().GetStringAsync("https://localhost:7131/api/FilterValue/GetFilterItems");
             var response = await _httpClient.GetFromJsonAsync<FilterItems>($"/api/FilterValue/GetFilterItems");
-
-            //var response = await httpClient.GetStringAsync("/api/FilterValue/GetFilterItems");
-
-            // string response = "{\"loBs\":[{\"name\":\"Wireless\",\"subLoBs\":[\"Wireless\",\"IoT\",\"Mobile Data\"]},{\"name\":\"Wireline\",\"subLoBs\":[\"Home Internet\",\"Home Television\",\"Home Phone\",\"SMB Internet\"]}],\"locations\":[\"Canada\"],\"brands\":[\"Bell\",\"Virgin\",\"Lucky Mobile\"],\"rebateTypes\":[\"Commission\",\"DownPayment\",\"Tax\"]}";
-
-            //var path = Directory.GetFiles("C:\\Users\\Public");
-            //using (StreamReader sr = File.OpenText("C:\\Users\\Public\\filteritems.txt"))
-            //{
-            //    response = sr.ReadToEnd();
-            //}
-            // var response = File.ReadAllText("filteritems.txt");
-            //FilterItems filterItems = new FilterItems();
-
-            //if (!string.IsNullOrEmpty(response))
-            //{
-            //    filterItems = response.JsonDeserialize<FilterItems>();
-            //}
-
+            //var response = await _httpClient.GetFromJsonAsync<FilterItems>($"https://dev.tools.staples.ca/BellServices/Reconciliation/api/FilterValue/GetFilterItems");
             return response;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public async Task<string> GetHello()
+    {
+        try
+        {
+            //var response = await new HttpClient().GetStringAsync("https://localhost:7131/api/FilterValue/GetFilterItems");
+            var response = await _httpClient.GetFromJsonAsync<FilterItems>($"/api/FilterValue/GetFilterItems");
+            //var response = await _httpClient.GetFromJsonAsync<string>($"https://dev.tools.staples.ca/BellServices/Reconciliation/api/FilterValue/GetFilterItems");
+            return JsonSerializer.Serialize(response);
         }
         catch (Exception ex)
         {
