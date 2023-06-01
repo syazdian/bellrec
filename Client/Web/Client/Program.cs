d7700b693c678143929b80f579e2670bc9863255
Sharif Yazdian <Sharif.Yazdian@staples.ca>
2023-06-01 13:06:07 -0400

    retreive

--- Client/Web/Client/Program.cs
+++ Client/Web/Client/Program.cs
@@ -12,11 +12,11 @@ public class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         // builder.Services.AddScoped<DialogService>();
-        //var baseAddress = builder.HostEnvironment.BaseAddress;
+        var baseAddress = builder.HostEnvironment.BaseAddress;
         //var baseAddress = "https://dev.tools.staples.ca/BellServices/Reconciliation/";
 
-        //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-        builder.Services.AddScoped(sp => new HttpClient { });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+        // builder.Services.AddScoped(sp => new HttpClient { });
         builder.Services.AddSqliteWasmDbContextFactory<StapleSourceContext>(opts => opts.UseSqlite("Data Source=StapleSource.sqlite3"));
 
         builder.Services.AddTransient<IFilterService, FilterService>();
